8bba2097951a7288f5938b6466c2a73fe1ff8976
BbeatrizSI <bbeatriz@gmail.com>
2025-02-06 16:31:39 +0100

    working on themes

--- src/app/products/features/product-detail/product-detail.component.ts
+++ src/app/products/features/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject, Input } from '@angular/core';
 import { ProductDetailStateService } from '../../data-access/product-detail-state.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CartStateService } from '../../../shared/data-access/cart-state.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,6 +12,7 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
 })
 export default class ProductDetailComponent implements OnInit {
   productDetailState = inject(ProductDetailStateService).state;
+  cartState = inject(CartStateService).state;
   
   @Input() id!: string;  // Se usa `@Input()` en lugar de `input.required`
 
@@ -20,5 +22,12 @@ export default class ProductDetailComponent implements OnInit {
     if (this.id) {
       this.productDetailState.getById(this.id);
     }
+  }
+
+  addToCart() {
+    this.cartState.add({
+      product: this.productDetailState.product()!,
+      quantity: 1
+    });
   }
 }
--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartStateService } from '../../data-access/cart-state.service';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,33 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styles: ``
 })
 export class HeaderComponent {
+  cartState = inject(CartStateService).state;
 
+  ngOninit(): void {
+    this.loadTheme();
+  }
+
+  toggleTheme() {
+    const html = document.documentElement;
+
+    if (html.classList.contains('dark')) {
+      html.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      console.log('light');
+      
+    } else {
+      html.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      console.log('dark');
+    }
+  }
+
+  loadTheme() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
