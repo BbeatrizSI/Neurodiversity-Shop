994cf9b7a53be1c0fa9d036b89a3e0805e44dc4e
BbeatrizSI <bbeatriz@gmail.com>
2025-02-11 15:52:43 +0100

    Auto-generated commit

---
+++ neurodiversity-shop/browser/chunk-4ZTOFTR5.js
@@ -0,0 +1,1 @@
+import"./chunk-2VMXMS7J.js";var o=[{path:"",loadComponent:()=>import("./chunk-YBF3MZGU.js")},{path:"product/:id",loadComponent:()=>import("./chunk-WORDTSUK.js")}];export{o as default};
---
+++ neurodiversity-shop/browser/chunk-PDZKS2SH.js
@@ -0,0 +1,1 @@
+import"./chunk-2VMXMS7J.js";var o=[{path:"",loadComponent:()=>import("./chunk-2SDYGF4Y.js")}];export{o as default};
